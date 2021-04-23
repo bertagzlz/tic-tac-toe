312857ca70a613d75b65e2be6c5f7998d722f858
bertagzlz <bertagzlz@gmail.com>
2021-04-23 17:33:14 +0200

    Updates

---
+++ precache-manifest.2f40a855ee72fcb91aa085fffdfdb477.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a9664fe450dd35ff0d7a441d697d3bd",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "460f4a7dc361573a24c2",
+    "url": "/tic-tac-toe/static/css/main.12a04e80.chunk.css"
+  },
+  {
+    "revision": "4bef887f27d2a25abc22",
+    "url": "/tic-tac-toe/static/js/2.ea69cc7d.chunk.js"
+  },
+  {
+    "revision": "460f4a7dc361573a24c2",
+    "url": "/tic-tac-toe/static/js/main.8a26debc.chunk.js"
+  },
+  {
+    "revision": "4b0deb1dd7866bc6d228",
+    "url": "/tic-tac-toe/static/js/runtime-main.c812f63a.js"
+  }
+]);
